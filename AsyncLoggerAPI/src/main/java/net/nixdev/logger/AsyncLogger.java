5b44b213ac1e14be5af9669741875aad9509fbd4
Pavel_Vinogradov <Pavel.Vinogradov@nixdev.net>
2013-06-09 21:15:16 +0400

    Initial project commit

---
+++ AsyncLoggerAPI/src/main/java/net/nixdev/logger/AsyncLogger.java
@@ -0,0 +1,9 @@
+package net.nixdev.logger;
+
+/**
+ * Created by Pavel_Vinogradov on 6/9/13.
+ */
+public interface AsyncLogger {
+
+    public void logMessage(String message);
+}
---
+++ AsyncLoggerSample/src/main/java/SampleLogger.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Pavel_Vinogradov on 6/9/13.
+ */
+public class SampleLogger implements AsyncLogger {
+}
